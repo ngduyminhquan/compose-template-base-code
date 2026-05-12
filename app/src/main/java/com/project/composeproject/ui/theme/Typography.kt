c3ba20b37f1937fc8c9b94a89bd202e6842d215d
quanndm <quanndm@infinitytech.global>
2026-05-12 17:49:54 +0700

    refactor: rename Type.kt to Typography.kt and add Roboto font support

---
+++ app/src/main/java/com/project/composeproject/ui/theme/Typography.kt
@@ -0,0 +1,7 @@
+package com.project.composeproject.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
+import com.project.composeproject.R
+
+val DefaultFont = Font(R.font.roboto_regular, FontWeight.Normal)
